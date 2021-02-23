65162d24442057e2aaa7c0290e4e91f21786a09f
Subasish Mohapatra <subasish.mohapatra@hotmail.com>
2021-02-23 21:34:44 +0530

    Test build pipeline

--- ECommerce.Api.Search/Program.cs
+++ ECommerce.Api.Search/Program.cs
@@ -11,6 +11,7 @@ namespace ECommerce.Api.Search
 {
     public class Program
     {
+        //Entry point
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
