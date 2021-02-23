615a3823073207c9181e926d43abc5b5371006a2
Subasish Mohapatra <subasish.mohapatra@hotmail.com>
2021-02-23 20:48:29 +0530

    Removed unecessary references

--- ECommerce.Api.Search/Startup.cs
+++ ECommerce.Api.Search/Startup.cs
@@ -1,17 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using ECommerce.Api.Search.Interfaces;
 using ECommerce.Api.Search.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Polly;
+using System;
 
 namespace ECommerce.Api.Search
 {
