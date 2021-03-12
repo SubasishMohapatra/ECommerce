2b905968fd3f050899189277c7bef5530422524b
Subasish Mohapatra <subasish.mohapatra@hotmail.com>
2021-03-12 18:21:55 +0530

    Checking if pipelines work

--- ECommerce.Api.Customers/Startup.cs
+++ ECommerce.Api.Customers/Startup.cs
@@ -1,17 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
 using ECommerce.Api.Customers.Db;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace ECommerce.Api.Customers
 {
